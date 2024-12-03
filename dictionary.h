20f8d678cdc1fc27a9033091db017718092ef015
Junior <barroso.junior@outlook.com>
2024-12-03 12:24:54 -0400

    init_repository

---
+++ dictionary.h
@@ -0,0 +1,7 @@
+/* Aluno: Marinaldo da Costa Barroso Filho
+   Curso: Ciência da Computação
+   Trabalho 1: Dicionário de Verbos irregulares Inglês-Português organizado com uma Lista */
+#define DICTIONARY_H
+#ifdef DICTIONARY_H
+
+#endif
